f0779ea4bb2fc5109598e34ae2781ccbca39f2d0
Michael Angerman <stormasm@gmail.com>
2020-09-28 21:02:54 -0700

    flexi_logger is UP and RUNNING !!

--- v20.0/tmp00/main.rs
+++ v20.0/tmp00/main.rs
@@ -1,8 +1,18 @@
 use nu_cli::create_default_context;
 use std::error::Error;
 
+use flexi_logger::Logger;
+use log::*;
+
 fn main() -> Result<(), Box<dyn Error>> {
     let context = create_default_context(true)?;
+
+    Logger::with_env().start().unwrap();
+    error!("error log");
+    warn!("warn log");
+    info!("info log");
+    debug!("debug log");
+    trace!("trace log");
 
     #[cfg(feature = "rustyline-support")]
     {
